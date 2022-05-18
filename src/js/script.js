1b1334aab3d497eccad5e8ec2b2c221d29393465
Vanathy98 <vanathythavayogarajah@gmail.com>
2022-05-17 22:30:38 -0700

    landing page created

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// Custome-Checkbox
+function onlyOne(checkbox) {
+  var checkboxes = document.getElementsByName("check");
+  checkboxes.forEach((item) => {
+    if (item !== checkbox) item.checked = false;
+  });
+}
